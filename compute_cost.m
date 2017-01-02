061082c73981043df1cc53ce618f767e134075eb
Baptiste Rouger <rouger.b@gmail.com>
2017-01-02 18:35:35 +0100

    Q9 done, Q10 in progress

--- compute_cost.m
+++ compute_cost.m
@@ -1,11 +1,11 @@
 function cost= compute_cost(k,m, tspan, x0, pe_ref, data) 
 
-.. %here define pe using pe_ref and k 
-..
+pe = pe_ref; %here define pe using pe_ref and k 
+pe([9 10]) = k';
 
 for i=1:length(m)
-    .. %here pe stores the ith inducer concentration
-    [t x]= ode15s(..);
-    sim(i,1)= .. ; %store the value at steady state of a chosen variable (either N, E, or A) 
+    pe(8) = m(i); %here pe stores the ith inducer concentration
+    [t x]= ode15s(@you_odeRI,tspan, x0, [], pe);
+    sim(i,1)= x(end,3); %store the value at steady state of a chosen variable (either N, E, or A) 
 end
-cost= .. %here you need to define the cost
+cost= sqrt(sum(( sim - data ).^2)/length(m)); %here you need to define the cost
--- you_odeR.m
+++ you_odeR.m
@@ -5,6 +5,6 @@ function xdot= you_odeR(t,x,pe)
 %eta is pe(10);
 xdot= zeros(size(x));
 xdot(1) = pe(1)*x(1) * (1-x(1)/pe(2)) - (pe(3)*x(2)*x(1));
-xdot(2) = 0.2*pe(6) + (5*pe(6)*((pe(8).^pe(10))/(pe(9).^pe(10) + pe(8).^pe(10))))*x(3) - pe(5)*x(2);
+xdot(2) = (0.2*pe(6) + 5*pe(6)*((pe(8).^pe(10))/(pe(9).^pe(10) + pe(8).^pe(10))))*x(3) - pe(5)*x(2);
 xdot(3) = pe(6)*x(1) - pe(7)*x(3);
 end
